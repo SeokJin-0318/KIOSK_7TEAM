7ddc8903922b5998633ee2888d6f2302ec18cae7
Darwin <limsj318@naver.com>
2023-12-29 12:07:25 +0900

    dev와 병합

---
+++ KIOSK/KIOSK/Cells/CartCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartCell.swift
+//  KIOSK
+//
+//  Created by 석진 on 12/29/23.
+//
+
+import UIKit
+
+class CartCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ KIOSK/KIOSK/Model/Cart.swift
@@ -0,0 +1,15 @@
+//
+//  Cart.swift
+//  KIOSK
+//
+//  Created by 석진 on 12/29/23.
+//
+
+import Foundation
+
+struct Cart
+{
+    var index: Int
+    var menuName: String
+    var menuPrice: Int
+}
