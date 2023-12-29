198b501716d5aa18a26027e5a6adbd73e041fd5e
gyeongeun <club2548@gmail.com>
2023-12-29 21:44:34 +0900

    쓸데없는 부분(UIButton: 경은 작성) 삭제

--- KIOSK/KIOSK/ViewController.swift
+++ KIOSK/KIOSK/ViewController.swift
@@ -14,19 +14,6 @@ class ViewController: UIViewController {
     
     // 1. 카테고리(UIStackView) : 임시로 UIButton만으로 구성하였음.
     // 각 버튼들은 HamburgerMenus, SideMenus, DrinkMenus 중 해당하는 Structure를 불러와 menus에 할당하고, menuCollection(메뉴화면)을 reload한다.
-    @IBAction func buttonHamburger(_ sender: UIButton) {
-        menus = HamburgerMenus()
-        menuCollection.reloadData()
-    }
-    @IBAction func buttonSide(_ sender: UIButton) {
-        menus = SideMenus()
-        menuCollection.reloadData()
-    }
-    @IBAction func buttonDrink(_ sender: UIButton) {
-        menus = DrinkMenus()
-        menuCollection.reloadData()
-    }
-    
     // 2.메뉴화면(이미지, 이름, 가격)을 구성하는 menuCollection(UICollectionView)과
     // 3.장바구니(이름, 수량, 가격, +,-버튼)을 구성하는 cart(UITableView).
     // 4.결제여부를 결정하는 UIButton과 UIAlertController.
